07aef52528421601466696a7aa06b6826f0d6b6d
BRGodara <83273724+BRGodara@users.noreply.github.com>
2021-04-27 13:50:51 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,307 @@
+function CutX(){
+	var CutX = document.getElementById("X");
+	var CutY = document.getElementById("Info");
+	CutX.style.display = "none";
+	CutY.style.display = "none";
+}
+
+function Clicked(Num){
+	var Data = "A" + Num;
+	
+	var ShowX = document.getElementById("X");
+	var ShowY = document.getElementById("Info");
+	var RepCoN = document.getElementById("CouN");
+	var RepCaN = document.getElementById("CapN")
+	
+	if(Data == "A1"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Maldive";
+		RepCaN.innerText = "Male";
+	}else if(Data == "A2"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Shree Lanka";
+		RepCaN.innerText = "Shree Jayvardhanpura";
+	}else if(Data == "A3"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "India";
+		RepCaN.innerText = "New Delhi";
+	}else if(Data == "A4"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Pakistan";
+		RepCaN.innerText = "Islamabad";
+	}else if(Data == "A5"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Afghanistan";
+		RepCaN.innerText = "Kabul";
+	}else if(Data == "A6"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Nepal";
+		RepCaN.innerText = "Kathmandu";
+	}else if(Data == "A7"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Bhutan";
+		RepCaN.innerText = "Thimpu";
+	}else if(Data == "A8"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Bangladesh";
+		RepCaN.innerText = "Dhaka";
+	}else if(Data == "A9"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Myanmar";
+		RepCaN.innerText = "Nepidow";
+	}else if(Data == "A10"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Thailand";
+		RepCaN.innerText = "Bangkok";
+	}else if(Data == "A11"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Laos";
+		RepCaN.innerText = "Vientiane";
+	}else if(Data == "A12"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Cambodia";
+		RepCaN.innerText = "Nempeng";
+	}else if(Data == "A13"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Vietnam";
+		RepCaN.innerText = "Hanoi";
+	}else if(Data == "A14"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Malaysia";
+		RepCaN.innerText = "Kual Lumpur";
+	}else if(Data == "A15"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Singapore";
+		RepCaN.innerText = "Singapore City";
+	}else if(Data == "A16"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Brunei";
+		RepCaN.innerText = "Bandar Siri Bagvan";
+	}else if(Data == "A17"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Indonesia";
+		RepCaN.innerText = "Jakarta";
+	}else if(Data == "A18"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Philippines";
+		RepCaN.innerText = "Manila";
+	}else if(Data == "A19"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Timor Liste";
+		RepCaN.innerText = "Dili";
+	}else if(Data == "A20"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Taiwan";
+		RepCaN.innerText = "Taipei";
+	}else if(Data == "A21"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "China";
+		RepCaN.innerText = "Beijing";
+	}else if(Data == "A22"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Mongolia";
+		RepCaN.innerText = "Ulaanbaatar";
+	}else if(Data == "A23"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "North Korea";
+		RepCaN.innerText = "Pyongyang";
+	}else if(Data == "A24"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "South Korea";
+		RepCaN.innerText = "Seoul";
+	}else if(Data == "A25"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Japan";
+		RepCaN.innerText = "Tokyo";
+	}else if(Data == "A26"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Kazakhstan";
+		RepCaN.innerText = "Astana";
+	}else if(Data == "A27"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Kirghistan";
+		RepCaN.innerText = "Bishkek";
+	}else if(Data == "A28"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Tajikistan";
+		RepCaN.innerText = "Dushanbe";
+	}else if(Data == "A29"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Turkmenistan";
+		RepCaN.innerText = "Ashgabat";
+	}else if(Data == "A30"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Uzbekistan";
+		RepCaN.innerText = "Tashkent";
+	}else if(Data == "A31"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Georgia";
+		RepCaN.innerText = "Tbilisi";
+	}else if(Data == "A32"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Armenia";
+		RepCaN.innerText = "Yerevan";
+	}else if(Data == "A33"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Azerbaijan";
+		RepCaN.innerText = "Baku";
+	}else if(Data == "A34"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Turkey";
+		RepCaN.innerText = "Angora";
+	}else if(Data == "A35"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Cyprus";
+		RepCaN.innerText = "Nicosia";
+	}else if(Data == "A36"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Syria";
+		RepCaN.innerText = "Damascus";
+	}else if(Data == "A37"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Lebanon";
+		RepCaN.innerText = "Beirut";
+	}else if(Data == "A38"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Israel";
+		RepCaN.innerText = "Jerusalem";
+	}else if(Data == "A39"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Philistine";
+		RepCaN.innerText = "Ramlla";
+	}else if(Data == "A40"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Jordan";
+		RepCaN.innerText = "Amman";
+	}else if(Data == "A41"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Saudi Arabia";
+		RepCaN.innerText = "Riyadh";
+	}else if(Data == "A42"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Yaman";
+		RepCaN.innerText = "Sana";
+	}else if(Data == "A43"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Oman";
+		RepCaN.innerText = "mascot";
+	}else if(Data == "A44"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "United Arab Emirates";
+		RepCaN.innerText = "Abu Dhabi";
+	}else if(Data == "A45"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Qatar";
+		RepCaN.innerText = "Doha";
+	}else if(Data == "A46"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Behrain";
+		RepCaN.innerText = "Manama";
+	}else if(Data == "A47"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Kuwait";
+		RepCaN.innerText = "Kuwait City";
+	}else if(Data == "A48"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Iraq";
+		RepCaN.innerText = "Baghdad";
+	}else if(Data == "A49"){
+		ShowX.style.display = "block";
+		ShowY.style.display = "block";
+		RepCoN.innerText = "Iran";
+		RepCaN.innerText = "Tehran";
+	}
+	
+	
+	
+}
+
+function Show_Map(){
+	var Main = document.getElementById("Main");
+	var Map = document.getElementById("Full_Map");
+	var Copyright_Info = document.getElementById("Copyright_Information");
+	Main.style.display = "none";
+	Map.style.display = "block";
+	Copyright_Info.style.display = "none";
+}
+
+function Cut_Map(){
+	var Main = document.getElementById("Main");
+	var Map = document.getElementById("Full_Map");
+	var Copyright_Info = document.getElementById("Copyright_Information");
+	Main.style.display = "block";
+	Map.style.display = "none";
+	Copyright_Info.style.display = "block";
+}
+
+function Next(){
+	var Next = document.getElementById("Next");
+	var Back = document.getElementById("Back");
+	var Png = document.getElementById("MapDotpng");
+	var Gif = document.getElementById("MapDotgif");
+	Png.style.display = "none";
+	Gif.style.display = "block";
+	Next.style.display = "none";
+	Back.style.display = "block";
+}
+
+function Back(){
+	var Next = document.getElementById("Next");
+	var Back = document.getElementById("Back");
+	var Png = document.getElementById("MapDotpng");
+	var Gif = document.getElementById("MapDotgif");
+	Png.style.display = "block";
+	Gif.style.display = "none";
+	Next.style.display = "block";
+	Back.style.display = "none";
+}
+
+
